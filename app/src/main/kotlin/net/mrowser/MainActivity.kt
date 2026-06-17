4c55cb53b199a2cc8074cbe05c567923370007a6
Mohammad Salehivaziri <mohammadsalehivaziri@gmail.com>
2026-06-18 00:03:39 +0330

    feat: add url entry bar and webview navigation

--- app/src/main/kotlin/net/mrowser/MainActivity.kt
+++ app/src/main/kotlin/net/mrowser/MainActivity.kt
@@ -4,26 +4,50 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+import net.mrowser.web.UrlNormalizer
 
 class MainActivity : Activity() {
 
     private lateinit var webView: WebView
+    private lateinit var urlInput: EditText
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        webView = WebView(this).apply {
-            webViewClient = WebViewClient()
-            settings.javaScriptEnabled = true
-            settings.domStorageEnabled = true
-            settings.mediaPlaybackRequiresUserGesture = false
-            settings.loadWithOverviewMode = true
-            settings.useWideViewPort = true
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        urlInput = findViewById(R.id.urlInput)
+        val goButton = findViewById<Button>(R.id.goButton)
+
+        webView.webViewClient = WebViewClient()
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            mediaPlaybackRequiresUserGesture = false
+            loadWithOverviewMode = true
+            useWideViewPort = true
         }
-        setContentView(webView)
-        webView.loadData(WELCOME_HTML, "text/html", "utf-8")
+
+        goButton.setOnClickListener { loadFromInput() }
+        urlInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                loadFromInput()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun loadFromInput() {
+        val url = UrlNormalizer.normalize(urlInput.text.toString()) ?: return
+        webView.loadUrl(url)
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
@@ -32,12 +56,5 @@ class MainActivity : Activity() {
             return true
         }
         return super.onKeyDown(keyCode, event)
-    }
-
-    companion object {
-        private const val WELCOME_HTML =
-            "<html><body style='background:#111;color:#eee;font-family:sans-serif;" +
-            "display:flex;height:100vh;margin:0;align-items:center;justify-content:center'>" +
-            "<h1>mrowser</h1></body></html>"
     }
 }
