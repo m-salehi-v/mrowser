7779a20151bd7dd30a0163890319d40f3851d871
Mohammad Salehivaziri <mohammadsalehivaziri@gmail.com>
2026-06-24 18:39:06 +0330

    chore: bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "net.mrowser"
         minSdk = 23
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     signingConfigs {
