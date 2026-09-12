23e7e6e3e0f4debd0157ff2db612b9b2facaf47f
Mohammad Salehivaziri <mohammadsalehivaziri@gmail.com>
2026-09-12 16:49:11 +0330

    chore: bump to 1.2.1 (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "net.mrowser"
         minSdk = 23
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.2.0"
+        versionCode = 7
+        versionName = "1.2.1"
     }
 
     signingConfigs {
