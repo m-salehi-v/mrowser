f061173fd8a39a6b3cd94689798fee02a825cf42
Mohammad Salehivaziri <mohammadsalehivaziri@gmail.com>
2026-09-19 01:47:09 +0330

    chore: bump to 1.3.0 (versionCode 8)
    Ad blocking (#38/#41) and DASH/progressive handoff (#42) since 1.2.1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "net.mrowser"
         minSdk = 23
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.2.1"
+        versionCode = 8
+        versionName = "1.3.0"
     }
 
     signingConfigs {
